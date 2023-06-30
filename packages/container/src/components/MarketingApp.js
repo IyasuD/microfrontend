cd7b006fbc376c9519fc50f4ff42d420bafb33c5
iyasud <iyasu.geleta@gmail.com>
2023-06-30 14:47:59 -0600

    adding routin bothe browserhistory and memory history

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,24 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
-
+import { useHistory } from "react-router-dom";
 //name of a component can be optional
 //makes the container independent from library/framework
 //makes decoupling easy
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // console.log("The container noticed navigation in marketing");
+        //console.log(nextPathname);
+        //prevent infinite update
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) history.push(nextPathname);
+      },
+    });
+   history.listen(onParentNavigate)
+
   }, []);
   return <div ref={ref} />;
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Landing from "./components /Landing";
 import Pricing from "./components /Pricing";
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
 });
-export default () => {
+export default ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        {/* <BrowserRouter> - reason it makes a browser history object*/}
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+          {/* </BrowserRouter> */}
+        </Router>
       </StylesProvider>
     </div>
   );
