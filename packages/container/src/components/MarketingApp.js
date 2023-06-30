0082bf27c73aa8a47a6405245d263e2db38de01f
iyasud <iyasu.geleta@gmail.com>
2023-06-29 18:55:13 -0600

    adding more comments

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from "react";
 
 //name of a component can be optional
 //makes the container independent from library/framework
+//makes decoupling easy
 export default () => {
   const ref = useRef(null);
   useEffect(() => {
