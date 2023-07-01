6ff4e354bd156531c0907b377f4f5fe79a6b196b
iyasud <iyasu.geleta@gmail.com>
2023-06-30 20:47:18 -0600

    adding auth deployment script

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 //name of a component can be optional
 //makes the container independent from library/framework
 //makes decoupling easy
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
   useEffect(() => {
@@ -16,6 +16,10 @@ export default () => {
         //prevent infinite update
         const { pathname } = history.location;
         if (pathname !== nextPathname) history.push(nextPathname);
+      },
+      onSignIn: () => {
+        //console.log("user signed in");
+        onSignIn();
       },
     });
     history.listen(onParentNavigate);
